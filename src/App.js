08676590330cc5b9976bff0e120a43e61afc9cd5
fedi1919 <fedi.hammami.stic@gmail.com>
2022-06-06 17:51:45 +0200

    rendering user places

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
+import UserPlaces from "./places/pages/UserPlaces";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Users />} exact />
           <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="/:userId/places" element={<UserPlaces />} exact />
         </Routes>
       </main>
     </BrowserRouter>
