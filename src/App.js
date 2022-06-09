84a6a089d7d72b1dc79cb4b4c34be17fbbb1b293
fedi1919 <fedi.hammami.stic@gmail.com>
2022-06-09 02:18:37 +0200

    adding ignup + switch mode state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/places/new" element={<NewPlace />} exact />
           <Route path="/:userId/places" element={<UserPlaces />} exact />
           <Route path="/places/:placeId" element={<UpdatePlace />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </main>
     </BrowserRouter>
